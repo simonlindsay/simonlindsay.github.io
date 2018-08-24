2592321c9bd8e719ff542b6649cf875fa2de8e3a
Simon Lindsay <simonlindsay@gmail.com>
2018-08-24 11:18:10 -0700

    initial import

---
+++ main.js
@@ -0,0 +1,6 @@
+import "./lib/portfolio-page-home.js";
+import "./lib/portfolio-page-project.js";
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.body.style.opacity = 1;
+});
